2228caa00e9c60d57ea8707960c3e83b77b1638a
Yuu Jinnai <yjinnai@cs.brown.edu>
2017-07-01 12:52:52 -0400

    Sat Jul  1 12:52:51 EDT 2017 pdf autocommitted: ../results/summary/jinnai-0.95.a5

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
 
 # TODO: Run parallel jobs.
+RUN="run-grid.sh"
 
-simple () {
-    samples=200
-    features=50
-    r0=0.5
-    np=2
-    instance="synth_${samples}_${features}_${r0}"
-    method="jinnai-0.95"
-    RESULT=../results/$instance.${method}.np$np.a5.stat
-    echo "qsub -pe orte $np -v inst=\"$instance\",method=\"${method}\",np=\"$np\" -e \"../results/$instance.${method}.np$np.a5.e\" -o \"$RESULT\"  run.sh"
-    if [ -f $RESULT ]
-       rm $RESULT
-    fi
-    qsub -pe orte $np -v instance="$instance",method="${method}",np="$np" -e "../results/$instance.${method}.np$np.a5.e" -o "$RESULT"  run.sh
-}
-simple
-exit
-
-RUN="run-raiden.sh"
 
 testsummary () {
     vsamples=200
-    vfeatures="50 100"
+    vfeatures="50 100 110 120 130 140 150 160 170 180 190 200"
+    vfeatures="50"
     rs="0.5"
     jids=""
     results=""
     method="jinnai-0.95"
     summary=../results/summary/${method}.a5
 #    nps="4 8 16"
-    nps="4"
+#    nps="2 4 8 16"
+    nps="2"
     for np in $nps
     do
 	for samples in $vsamples
@@ -41,7 +26,7 @@ testsummary () {
 		do
 		    instance="synth_${samples}_${features}_${r0}"
 		    RESULT=../results/$instance.${method}.np$np.a5.stat
-		    id=`qsub -pe impi $np -v instanced=$instance,method=${method},np=$np -e "../results.$instance.${method}.np$np.a5.e" -o "$RESULT"  $RUN | awk '{print $3}'`
+		    id=`qsub -pe orte $np -v instanced=$instance,method=${method},np=$np -e "../results.$instance.${method}.np$np.a5.e" -o "$RESULT"  $RUN | awk '{print $3}'`
 		    jids="$jids,$id"
 		    results="${results} ${RESULT}"
 		done
@@ -55,6 +40,24 @@ testsummary () {
     qsub -v results="$results",summary="$summary" \
 	-hold_jid $jids summary.sh
 }
-#testsummary
-#exit
+testsummary
+exit
 
+
+simple () {
+    samples=200
+    features=100
+    r0=0.5
+    np=2
+    instance="synth_${samples}_${features}_${r0}"
+    method="jinnai-0.95"
+    RESULT=../results/$instance.${method}.np$np.a5.stat
+    echo "qsub -pe orte $np -v inst=\"$instance\",method=\"${method}\",np=\"$np\" -e \"../results/$instance.${method}.np$np.a5.e\" -o \"$RESULT\"  $RUN"
+    if [ -f "$RESULT" ]
+    then
+       rm $RESULT
+    fi
+    qsub -pe orte $np -v instance="$instance",method="${method}",np="$np" -e "../results/$instance.${method}.np$np.a5.e" -o "$RESULT"  $RUN
+}
+simple
+exit
